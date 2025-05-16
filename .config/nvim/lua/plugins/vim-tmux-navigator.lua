b7eb99cfe324ea4d0806fc3143076c428fb1981d
Steve Jacobs <stephenjacobs86@gmail.com>
2025-05-16 16:37:38 -0400

    start moving back to tmux

---
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,19 @@
+return {}
+-- return {
+--   "christoomey/vim-tmux-navigator",
+--   cmd = {
+--     "TmuxNavigateLeft",
+--     "TmuxNavigateDown",
+--     "TmuxNavigateUp",
+--     "TmuxNavigateRight",
+--     "TmuxNavigatePrevious",
+--     "TmuxNavigatorProcessList",
+--   },
+--   keys = {
+--     { "<c-n>", "<cmd>TmuxNavigateLeft<cr>" },
+--     { "<c-e>", "<cmd>TmuxNavigateDown<cr>" },
+--     { "<c-u>", "<cmd>TmuxNavigateUp<cr>" },
+--     { "<c-i>", "<cmd>TmuxNavigateRight<cr>" },
+--     { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+--   },
+-- }
