bb87cf28244dd147aa081f993078fb104dfa79fa
Steve Jacobs <stephenjacobs86@gmail.com>
2025-05-22 09:42:03 -0400

    update snack picker config

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -8,6 +8,18 @@ return {
         files = {
           hidden = true,
           ignored = true,
+          exclude = { "**/node_modules/**" },
+        },
+      },
+    },
+    explorer = {
+      hidden = true,
+      ignored = true,
+      sources = {
+        files = {
+          hidden = true,
+          ignored = true,
+          exclude = { "**/node_modules/**" },
         },
       },
     },
