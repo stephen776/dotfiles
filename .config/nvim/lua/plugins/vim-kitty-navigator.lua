13dbbfeb2ec78ecfadb86bc6c3c9a01d47dc15a4
Steve Jacobs <stephenjacobs86@gmail.com>
2025-08-11 19:48:06 -0400

    update tmux

--- .config/nvim/lua/plugins/vim-kitty-navigator.lua
+++ .config/nvim/lua/plugins/vim-kitty-navigator.lua
@@ -1,47 +1,48 @@
-return {
-  "knubie/vim-kitty-navigator",
-  build = "cp ./*.py ~/.config/kitty/",
-  enabled = true,
-  keys = {
-    {
-      "<C-m>",
-      "<cmd>KittyNavigateLeft<cr>",
-      desc = "Navigate window left",
-    },
-    {
-      "<C-n>",
-      "<cmd>KittyNavigateDown<cr>",
-      desc = "Navigate window down",
-    },
-    {
-      "<C-e>",
-      "<cmd>KittyNavigateUp<cr>",
-      desc = "Navigate window up",
-    },
-    {
-      "<C-i>",
-      "<cmd>KittyNavigateRight<cr>",
-      desc = "Navigate window right",
-    },
-    {
-      "<C-h>",
-      "<cmd>KittyNavigateLeft<cr>",
-      desc = "Navigate window left",
-    },
-    {
-      "<C-j>",
-      "<cmd>KittyNavigateDown<cr>",
-      desc = "Navigate window down",
-    },
-    {
-      "<C-k>",
-      "<cmd>KittyNavigateUp<cr>",
-      desc = "Navigate window up",
-    },
-    {
-      "<C-l>",
-      "<cmd>KittyNavigateRight<cr>",
-      desc = "Navigate window right",
-    },
-  },
-}
+return {}
+-- return {
+--   "knubie/vim-kitty-navigator",
+--   build = "cp ./*.py ~/.config/kitty/",
+--   enabled = true,
+--   keys = {
+--     {
+--       "<C-m>",
+--       "<cmd>KittyNavigateLeft<cr>",
+--       desc = "Navigate window left",
+--     },
+--     {
+--       "<C-n>",
+--       "<cmd>KittyNavigateDown<cr>",
+--       desc = "Navigate window down",
+--     },
+--     {
+--       "<C-e>",
+--       "<cmd>KittyNavigateUp<cr>",
+--       desc = "Navigate window up",
+--     },
+--     {
+--       "<C-i>",
+--       "<cmd>KittyNavigateRight<cr>",
+--       desc = "Navigate window right",
+--     },
+--     {
+--       "<C-h>",
+--       "<cmd>KittyNavigateLeft<cr>",
+--       desc = "Navigate window left",
+--     },
+--     {
+--       "<C-j>",
+--       "<cmd>KittyNavigateDown<cr>",
+--       desc = "Navigate window down",
+--     },
+--     {
+--       "<C-k>",
+--       "<cmd>KittyNavigateUp<cr>",
+--       desc = "Navigate window up",
+--     },
+--     {
+--       "<C-l>",
+--       "<cmd>KittyNavigateRight<cr>",
+--       desc = "Navigate window right",
+--     },
+--   },
+-- }
--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,19 +1,18 @@
-return {}
--- return {
---   "christoomey/vim-tmux-navigator",
---   cmd = {
---     "TmuxNavigateLeft",
---     "TmuxNavigateDown",
---     "TmuxNavigateUp",
---     "TmuxNavigateRight",
---     "TmuxNavigatePrevious",
---     "TmuxNavigatorProcessList",
---   },
---   keys = {
---     { "<c-n>", "<cmd>TmuxNavigateLeft<cr>" },
---     { "<c-e>", "<cmd>TmuxNavigateDown<cr>" },
---     { "<c-u>", "<cmd>TmuxNavigateUp<cr>" },
---     { "<c-i>", "<cmd>TmuxNavigateRight<cr>" },
---     { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
---   },
--- }
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+  },
+}
